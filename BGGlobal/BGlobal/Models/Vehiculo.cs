05e1d036c539c92eb8faa57e7902f8ac6d525815
Alejandro <AlejandroFCasas@gmail.com>
2021-12-03 12:29:06 -0300

    segundo comit

--- BGGlobal/BGlobal/Models/Vehiculo.cs
+++ BGGlobal/BGlobal/Models/Vehiculo.cs
@@ -14,16 +14,22 @@ namespace BGlobal.Models
         public int IdVehiculo { get; set; }
 
         [StringLength(8, ErrorMessage = "Exceso de caracteres")]
+        [Required(ErrorMessage = "Patente requerido")]
+
         public String Patente { get; set; }
 
+        [Required(ErrorMessage = "Marca requerido")]
+        [Display(Name = "Marca")]
         public String Marca { get; set; }
-
+        [Required(ErrorMessage = "Modelo requerido")]
+        [Display(Name = "Modelo")]
         public String Modelo { get; set; }
 
 
         [Range(2, 5, ErrorMessage = "ingrese un valor de 2 a 5")]
         public int Puertas { get; set; }
-
+        [Required(ErrorMessage = "Titular requerido")]
+        [Display(Name = "Titular")]
         public String Titular { get; set; }
 
     }
